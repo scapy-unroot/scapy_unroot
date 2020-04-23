aa8c95ae85a66351655f8529fd6ecddab4dd54c6
Martine Lenders <mail@martine-lenders.eu>
2020-04-23 17:59:37 +0200

    tests: more unused code

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,16 +11,3 @@
 """
 scapy_unroot test suite
 """
-
-import unittest
-
-
-def test_all():
-    # use the default shared TestLoader instance
-    test_loader = unittest.defaultTestLoader
-    # use the basic test runner that outputs to sys.stderr
-    test_runner = unittest.TextTestRunner()
-    # automatically discover all tests in the current dir of the form test*.py
-    test_suite = test_loader.discover('.')
-    # run the test suite
-    test_runner.run(test_suite)

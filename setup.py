f60de78edfc89346993d7bfedad660cac2d16f59
Martine Lenders <mail@martine-lenders.eu>
2020-04-30 17:35:37 +0200

    setup.py: bump version for test and doc fixes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import sys
 
 
 name = "scapy_unroot"
-version = "0.3.0b1"
+version = "0.3.0b2"
 description = "Daemon and tooling to enable using scapy without " \
               "root permissions."
 author = "Martine S. Lenders"
